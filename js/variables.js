6d55c3b94c3241a168979e2d16928affb1f9db3b
kahverengi <ahmet@duruer.dev>
2025-01-25 01:21:02 +0300

    feat: add prerelease option

--- js/variables.js
+++ js/variables.js
@@ -3,6 +3,7 @@ export const UI_URL = import.meta.env.VITE_UI_URL;
 export const PANEL_URL = import.meta.env.VITE_PANEL_URL;
 export const SETUP_URL = import.meta.env.VITE_SETUP_URL;
 export const PANO_WEBSITE_URL = import.meta.env.VITE_PANO_WEBSITE_URL;
+export const PRERELEASE = import.meta.env.VITE_PRERELEASE;
 
 export const COOKIE_PREFIX = import.meta.env.VITE_COOKIE_PREFIX;
 
