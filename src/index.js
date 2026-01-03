14a936581387e7af430735d855ca2362a11f1f8d
kahverengi <ahmet@duruer.dev>
2026-01-03 15:44:56 +0300

    feat: added some test context

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
+import { setContext, getContext } from 'svelte'
+
 export { default as PanoTestButton } from './components/Button.svelte';
+
+export const PANO_CTX = Symbol()
+
+export function setPanoContext(ctx) {
+    setContext(PANO_CTX, ctx)
+}
+
+export function usePano() {
+    return getContext(PANO_CTX)
+}
