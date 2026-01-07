b21536ee4abe81e9d402638467aa50101fb5bb3c
kahverengi <ahmet@duruer.dev>
2026-01-07 22:35:47 +0300

    feat: added svelte stuff

---
+++ src/svelte.js
@@ -0,0 +1,6 @@
+import { getPanoContext } from "./internal/index.js";
+
+const panoContext = getPanoContext();
+const { page, base, navigating, browser } = panoContext.context;
+
+export { page, base, navigating, browser };
