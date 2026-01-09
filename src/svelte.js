625620550a314a13beb887f2e9761697689a313d
kahverengi <ahmet@duruer.dev>
2026-01-09 23:35:52 +0300

    feat: added goto function to sdk

--- src/svelte.js
+++ src/svelte.js
@@ -1,6 +1,6 @@
 import { getPanoContext } from './internal/index.js';
 
 const panoContext = getPanoContext();
-const { page, base, navigating, browser } = panoContext.context;
+const { page, base, navigating, browser, goto } = panoContext.context;
 
-export { page, base, navigating, browser };
+export { page, base, navigating, browser, goto };
